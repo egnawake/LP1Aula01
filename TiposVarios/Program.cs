543ea614358662ee9eddc93a8afc415d0bfcbab9
João Inácio <egnawake@gmail.com>
2022-02-24 09:40:04 +0000

    TiposVarios: add floats

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -18,6 +18,13 @@ namespace TiposVarios
             char arrow = '\u2192';
 
             Console.WriteLine($"Chars: {letterA}, {omega}, {arrow}");
+
+
+            float pi = 3.14f;
+            float floatA = 123.45f;
+            float floatB = 100.001f;
+
+            Console.WriteLine($"Floats: {pi}, {floatA}, {floatB}");
         }
     }
 }
