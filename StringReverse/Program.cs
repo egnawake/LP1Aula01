28069592f7abf2730e30eb9ba01e86e7ab8ae061
João Inácio <egnawake@gmail.com>
2022-02-24 11:58:13 +0000

    Complete bases_cs/ex11

---
+++ StringReverse/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StringReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // get user input
+            Console.WriteLine("Insert a string:");
+            string inp = Console.ReadLine();
+
+            // create string with input contents reversed
+            string reverse = "";
+            for (int i = 0; i < inp.Length; i++)
+            {
+                reverse = inp[i] + reverse;
+            }
+
+            // print reversed string
+            Console.WriteLine("Reversed input: " + reverse);
+        }
+    }
+}
