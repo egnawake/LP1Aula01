0abc74d7a98db8047a9ce4571a9ad7992ea4ac8a
João Inácio <egnawake@gmail.com>
2022-02-24 11:38:24 +0000

    Complete bases_cs/ex10

---
+++ Echo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Echo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert a string:");
+            string inp = Console.ReadLine();
+            
+            Console.WriteLine("You wrote " + inp);
+        }
+    }
+}
