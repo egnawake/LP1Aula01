8e4dc6f44b839749d06be38fba6448b304b9941f
João Inácio <egnawake@gmail.com>
2022-02-24 12:52:35 +0000

    Complete bases_cs/ex14

---
+++ VolumeOfParallelepiped/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace VolumeOfParallelepiped
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // get parameters from user
+            Console.Write("Enter length: ");
+            string lengthStr = Console.ReadLine();
+            Console.Write("Enter height: ");
+            string heightStr = Console.ReadLine();
+            Console.Write("Enter width: ");
+            string widthStr = Console.ReadLine();
+
+            // convert input to floats
+            float length = Convert.ToSingle(lengthStr);
+            float height = Convert.ToSingle(heightStr);
+            float width = Convert.ToSingle(widthStr);
+
+            // calculate volume
+            float volume = length * width * height;
+
+            // print result
+            Console.WriteLine($"Volume = {volume}");
+        }
+    }
+}
