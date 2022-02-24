397f0e00db9c4f94993964622af91d1b7fa0ecfd
João Inácio <egnawake@gmail.com>
2022-02-24 09:59:54 +0000

    Complete exercise 5

--- VariasStrings/Program.cs
+++ VariasStrings/Program.cs
@@ -7,18 +7,39 @@ namespace VariasStrings
         static void Main(string[] args)
         {
             string hello = "Hello world!";
-            string tabs = "One\tTwo\tThree";
-            string newline = "This string has\na newline";
-            string unicode = "\u2153 \u3042 \u232b";
-            string quotes = "\"Escaped quotes\"";
-            string verbatim = @"\n \t \u0101";
-
             Console.WriteLine(hello);
+            
+            string tabs = "One\tTwo\tThree";
             Console.WriteLine(tabs);
+
+            string newline = "This string has\na newline";
             Console.WriteLine(newline);
+
+            string unicode = "\u2153 \u3042 \u232b";
             Console.WriteLine(unicode);
+
+            string quotes = "\"Escaped quotes\"";
             Console.WriteLine(quotes);
+
+            string verbatim = @"\n \t \u0101";
             Console.WriteLine(verbatim);
+
+
+            string concat1 = "abc" + "def";
+            Console.WriteLine(concat1);
+
+            string concat2 = 2 + " + " + 5 + " = " + 7;
+            Console.WriteLine(concat2);
+
+            int x = 4;
+            int y = 10;
+            string interpolation1 = $"{x} - {y} = {x - y}";
+            Console.WriteLine(interpolation1);
+
+            string interpolation2 = String.Format("{0} * {1} = {2}", x, y, x * y);
+            Console.WriteLine(interpolation2);
+
+            Console.WriteLine("{0}^2 = {1}", x, x * x);
         }
     }
 }
