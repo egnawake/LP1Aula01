12ee149d671e9e1026df130c9e77bc06ab0ca346
João Inácio <egnawake@gmail.com>
2022-02-24 09:38:11 +0000

    TiposVarios: add integers

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int intA = 9;
+            int intB = 14;
+            int intC = 1024;
+
+            Console.WriteLine($"Integers: {intA}, {intB}, {intC}");
+        }
+    }
+}
