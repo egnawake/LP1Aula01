3c2796fff0792808a8da4fc397706135b82dc724
João Inácio <egnawake@gmail.com>
2022-02-24 01:47:39 +0000

    Complete exercise 2

---
+++ OlaMundo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OlaMundo
+{
+    /// <summary>
+    /// Main class.
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Writes "Hello World!" to the console.
+        /// </summary>
+        /// <param name="args">Command line arguments.</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
