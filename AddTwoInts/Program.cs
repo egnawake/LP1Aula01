05036dcfc7e410d2f1b82c7239b666e73580f52d
João Inácio <egnawake@gmail.com>
2022-02-24 12:18:53 +0000

    Complete bases_cs/ex12

---
+++ AddTwoInts/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AddTwoInts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ask user for two numbers
+            Console.Write("Enter first number: ");
+            string firstInput = Console.ReadLine();
+            Console.Write("Enter second number: ");
+            string secondInput = Console.ReadLine();
+
+            // parse ints from input
+            int firstNumber = int.Parse(firstInput);
+            int secondNumber = int.Parse(secondInput);
+            // calculate sum
+            int result = firstNumber + secondNumber;
+
+            // print result
+            Console.WriteLine($"{firstNumber} + {secondNumber} = {result}");
+        }
+    }
+}
